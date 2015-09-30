cb5bee2c66c4e8dec4fd8d23a21dca3cf3890a3d
Seunghoon Ko <imskojs@gmail.com>
2015-09-30 10:52:32 +0900

    kakao native login

--- codes/state/login/loginController.js
+++ codes/state/login/loginController.js
@@ -1,40 +1,41 @@
 (function() {
-    'use strict';
-    angular.module('app')
-        .controller('LoginController', LoginController);
+  'use strict';
+  angular.module('app')
+    .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['LoginModel', 'AuthService', 'Message', '$state'];
+  LoginController.$inject = ['LoginModel', 'AuthService', 'Message', '$state', '$window'];
 
-    function LoginController(LoginModel, AuthService, Message, $state) {
+  function LoginController(LoginModel, AuthService, Message, $state, $window) {
 
-        var Login = this;
-        Login.Model = LoginModel;
+    var Login = this;
+    Login.Model = LoginModel;
 
-        Login.loginWithFaceBook = loginWithFacebook;
-        Login.loginWithKaKaoTalk = loginWithKaKaoTalk;
-        //------------------------
-        //  IMPLEMENTATIONS
-        //------------------------
-        function loginWithFacebook() {
-            return AuthService.loginWithFacebook()
-                .then(function success(data) {
-                    console.log(data);
-                    Message.loading.hide();
-                    $state.go('main.home');
-                }, function err(error) {
-                    console.log(error);
-                });
-        }
-
-        function loginWithKaKaoTalk() {
-            return AuthService.loginWithKakao()
-                .then(function(data) {
-                    console.log(data);
-                    Message.loading.hide();
-                    $state.go('main.home');
-                }, function err(error) {
-                    console.log(error);
-                });
-        }
+    Login.loginWithFaceBook = loginWithFacebook;
+    Login.loginWithKaKaoTalk = loginWithKaKaoTalk;
+    //------------------------
+    //  IMPLEMENTATIONS
+    //------------------------
+    function loginWithFacebook() {
+      return AuthService.loginWithFacebook()
+        .then(function success(data) {
+          console.log(data);
+          Message.loading.hide();
+          $state.go('main.home');
+        }, function err(error) {
+          console.log(error);
+        });
     }
+
+    function loginWithKaKaoTalk() {
+
+      return AuthService.loginWithKakao()
+        .then(function(data) {
+          console.log(data);
+          Message.loading.hide();
+          $state.go('main.home');
+        }, function err(error) {
+          console.log(error);
+        });
+    }
+  }
 })();
