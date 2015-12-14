e67a2d39b79d0647cb5840ac21b9c02accf2a2da
박정현 <pjhyun25@bagjeonghyeon-ui-MacBook-Pro.local>
2015-12-14 18:40:01 +0900

    first commit

--- codes/state/main/mainModel.js
+++ codes/state/main/mainModel.js
@@ -20,27 +20,47 @@
                 // profile_image: "http://mud-kage.kakao.co.kr/14/dn/btqcfpt4vNR/Fmmz6xsVRH05wv1UYNniRK/o.jpg"
 
             },
-            sideMenuLists: [{
+            sideMenuLists: [
+
+            {   
+                icon: '../img/800/icon/home.png',
                 state: 'main.home',
                 text: '홈으로 가기'
-            }, {
-                state: 'main.show.savedList',
-                text: '마이갤러리'
-            }, {
-                state: 'main.balance.list',
-                text: '적립금 확인'
-            }, {
-                state: 'main.cancel',
-                text: '예약 취소'
-            }, {
+            },
+
+            {
+                icon: '../img/800/icon/bulb.png',
+                state: 'main.announcements.noticeList',
+                text: '공지사항'
+            },
+
+            {
+                icon: '../img/800/icon/present.png',
                 state: 'main.announcements.eventList',
-                text: '이벤트 / 공지사항'
-            }],
+                text: '이벤트'
+            }, 
+
+            // {
+            //     state: 'main.show.savedList',
+            //     text: '마이갤러리'
+            // }, 
+
+            // {
+            //     state: 'main.balance.list',
+            //     text: '적립금 확인'
+            // }, 
+
+            // {
+            //     state: 'main.cancel',
+            //     text: '예약 취소'
+            // }
+             ],
+
             currentItem: {
                 state: 'main.home',
                 text: '홈으로 가기'
             },
-            setting: {
+            setting: {                
                 pushNotification: true
             }
         };
