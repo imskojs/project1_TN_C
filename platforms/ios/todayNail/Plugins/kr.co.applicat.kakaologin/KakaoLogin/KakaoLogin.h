879520ba30f773a521988e1f84b0ff586a39de24
Seunghoon Ko <imskojs@gmail.com>
2016-05-19 13:11:55 +0900

    init

---
+++ platforms/ios/todayNail/Plugins/kr.co.applicat.kakaologin/KakaoLogin/KakaoLogin.h
@@ -0,0 +1,7 @@
+#import <Cordova/CDV.h>
+
+@interface KakaoLogin : CDVPlugin
+
+- (void) login:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ platforms/ios/todayNail/Plugins/kr.co.applicat.kakaologin/KakaoLogin/KakaoLogin.m
@@ -0,0 +1,19 @@
+#import "KakaoLogin.h"
+
+@implementation KakaoLogin
+
+- (void)login:(CDVInvokedUrlCommand*)command
+{
+
+    NSString* callbackId = [command callbackId];
+    NSString* name = [[command arguments] objectAtIndex:0];
+    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:msg];
+
+    [self success:result callbackId:callbackId];
+}
+
+@end
